9ce78bcc06af539b37e8725be78520d61140b63b
Bishop121 <obidikeemmanuel@outlook.com>
2024-10-18 16:41:14 +0000

    Old school band

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,12 @@
+-- 3 first students in the Batch ID=3
+-- because Batch 3 is the best!
+
+SELECT band_name, 
+       CASE 
+           WHEN split IS NULL THEN 2022 - formed
+           ELSE split - formed
+       END AS lifespan
+FROM metal_bands
+WHERE main_style = 'Glam rock'
+ORDER BY lifespan DESC;
+
